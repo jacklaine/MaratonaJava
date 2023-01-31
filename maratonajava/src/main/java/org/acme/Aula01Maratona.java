48031ce6ba8f0d1b2acb95ff3bda525885749721
Jacklaine <jacklainecavalheiro1@gmail.com>
2023-01-30 22:25:56 -0300

    Add files via upload

---
+++ maratonajava/src/main/java/org/acme/Aula01Maratona.java
@@ -0,0 +1,10 @@
+package org.acme;
+
+
+public class Aula01Maratona {
+
+    public static void main(String[] args) {
+        System.out.println("SHAZAN");
+    }
+
+}
---
+++ maratonajava/src/main/java/org/acme/Aula02TipoPrimitivo.java
@@ -0,0 +1,24 @@
+package org.acme;
+
+public class Aula02TipoPrimitivo {
+    public static void main(String[] args) {
+
+        int age = (int)10000000000L;
+        long numberLong = (long) 155.23;
+        double salaryDouble = 2000.0;
+        float salaryFloat = (float) 2500.0D;
+        byte ageByte = 127;
+        short ageShort = 32000;
+        boolean truee = true;
+        boolean falsee = false;
+        char caractere = 65;
+        String text = "estou cansada e com fome";
+
+        System.out.println(age);
+        System.out.println(truee);
+        System.out.println(caractere);
+        System.out.println("Mano, "+text);
+        
+    }
+    
+}
