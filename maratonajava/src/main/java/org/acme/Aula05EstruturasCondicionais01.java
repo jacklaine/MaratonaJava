b0a8c7d3162fe56c91f2d258c5ddee3c8ae30b70
jacklaine <jacklainecavalheiro1@gmail.com>
2023-02-01 22:08:33 -0300

    Day3

---
+++ maratonajava/src/main/java/org/acme/Aula05EstruturasCondicionais01.java
@@ -0,0 +1,26 @@
+package org.acme;
+
+public class Aula05EstruturasCondicionais01 {
+    public static void main(String[] args) {
+
+        int age = 15;
+        boolean isBuyCorote = age >= 18;
+
+        if (isBuyCorote) {
+            System.out.println("Pode comprar uns corote");
+        } else {
+            System.out.println("Proibido comprar corote");
+        }
+
+        if (isBuyCorote == false) {
+            System.out.println("Proibido comprar corote");
+        }
+
+        if (!isBuyCorote) {
+            System.out.println("Proibido comprar corote");
+        }
+
+        System.out.println("Fora do if"); 
+    }
+
+}
