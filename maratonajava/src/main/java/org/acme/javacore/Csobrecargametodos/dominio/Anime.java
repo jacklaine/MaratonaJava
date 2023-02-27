87ab159aea1fec406c1e4690e5a72aa9aacdeef8
jacklaine <jacklainecavalheiro1@gmail.com>
2023-02-27 20:26:36 -0300

    Sobrecarga Metodo

--- maratonajava/src/main/java/org/acme/javacore/Csobrecargametodos/dominio/Anime.java
+++ maratonajava/src/main/java/org/acme/javacore/Csobrecargametodos/dominio/Anime.java
@@ -2,9 +2,37 @@ package org.acme.javacore.Csobrecargametodos.dominio;
 
 public class Anime {
 
+    private String nome;
     private String tipo;
     private int episodios;
+    private String genero;
 
+    public void init(String nome, String tipo, int episodio) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodio;
+    }
+
+    public void init(String nome, String tipo, int episodio, String genero) {
+        this.init(nome, tipo, episodio);
+        this.genero = genero;
+    }
+
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.genero);
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
     public String getTipo() {
         return tipo;
     }
@@ -17,5 +45,13 @@ public class Anime {
     public void setEpisodios(int episodios) {
         this.episodios = episodios;
     }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    
 
 }
---
+++ maratonajava/src/main/java/org/acme/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package org.acme.javacore.Csobrecargametodos.test;
+
+import org.acme.javacore.Csobrecargametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        anime.init("Naruto", "TV", 12, "Ação");
+        anime.imprime();
+    }
+    
+}
