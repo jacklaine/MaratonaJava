0fb356b893e211812b0f8e4a9e1c55d8cb7b1c51
jacklaine <jacklainecavalheiro1@gmail.com>
2023-03-01 21:46:03 -0300

    Construtores

--- maratonajava/src/main/java/org/acme/javacore/Csobrecargametodos/test/AnimeTest01.java
+++ maratonajava/src/main/java/org/acme/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -4,8 +4,7 @@ import org.acme.javacore.Csobrecargametodos.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime();
-        anime.init("Naruto", "TV", 12, "Ação");
+        Anime anime = new Anime("Naruto", "TV", 12, "Ação", "Walt");
         anime.imprime();
     }
     
