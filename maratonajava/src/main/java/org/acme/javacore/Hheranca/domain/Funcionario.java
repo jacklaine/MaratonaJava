2172926bf23fdbec158ae0f2866a99483106d7a4
jacklaine <jacklainecavalheiro1@gmail.com>
2023-03-15 23:18:54 -0300

    Heranca

---
+++ maratonajava/src/main/java/org/acme/javacore/Hheranca/domain/Funcionario.java
@@ -0,0 +1,15 @@
+package org.acme.javacore.Hheranca.domain;
+
+public class Funcionario extends Pessoa {
+
+    private double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+}
---
+++ maratonajava/src/main/java/org/acme/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,28 @@
+package org.acme.javacore.Hheranca.test;
+
+import org.acme.javacore.Hheranca.domain.Endereco;
+import org.acme.javacore.Hheranca.domain.Funcionario;
+import org.acme.javacore.Hheranca.domain.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("85815570");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Maria");
+        pessoa.setCpf("11111");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Ana");
+        funcionario.setCpf("11111");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+        funcionario.imprime();
+
+    }
+
+}
