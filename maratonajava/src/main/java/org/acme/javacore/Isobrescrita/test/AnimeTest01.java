80abf9dcd59916ed8bb1e1034c4d869be57ef29c
jacklaine <jacklainecavalheiro1@gmail.com>
2023-03-22 20:08:49 -0300

    toString

---
+++ maratonajava/src/main/java/org/acme/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package org.acme.javacore.Isobrescrita.test;
+
+import org.acme.javacore.Isobrescrita.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+
+        System.out.println(anime);
+    }
+    
+}
