061e23f65af52cbc55af3efd6c9da786716135c9
jacklaine <jacklainecavalheiro1@gmail.com>
2023-04-13 19:51:14 -0300

    polimorfismo

--- maratonajava/src/main/java/org/acme/javacore/Npolimorfismo/test/ProdutoTest02.java
+++ maratonajava/src/main/java/org/acme/javacore/Npolimorfismo/test/ProdutoTest02.java
@@ -2,12 +2,20 @@ package org.acme.javacore.Npolimorfismo.test;
 
 import org.acme.javacore.Npolimorfismo.domain.Computador;
 import org.acme.javacore.Npolimorfismo.domain.Produto;
+import org.acme.javacore.Npolimorfismo.domain.Tomate;
 
 public class ProdutoTest02 {
     public static void main(String[] args) {
         Produto produto = new Computador("Ryzen 9", 3000);
         System.out.println(produto.getNome());
         System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+        System.out.println("-------------------");
+
+        Produto produto2 = new Tomate("Maçã", 2);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
+        System.out.println(produto2.calcularImposto());
     }
     
 }
