1de76dcd17ff8d0b812963cb4ecb3f86e8dbb45b
jacklaine <jacklainecavalheiro1@gmail.com>
2023-04-25 21:34:00 -0300

    erros

---
+++ maratonajava/src/main/java/org/acme/javacore/Npolimorfismo/test/StackOverFlowTest01.java
@@ -0,0 +1,13 @@
+package org.acme.javacore.Npolimorfismo.test;
+
+public class StackOverFlowTest01 {
+    public static void main(String[] args) {
+        recursividade();
+        
+    }
+
+    public static void recursividade() {
+        recursividade();
+    }
+    
+}
