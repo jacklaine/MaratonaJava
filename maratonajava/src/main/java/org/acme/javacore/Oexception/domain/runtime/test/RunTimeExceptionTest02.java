266d60ad4b8f05a4f01f7921714d312ebbe9ecf6
jacklaine <jacklainecavalheiro1@gmail.com>
2023-05-09 20:18:46 -0300

    IllegalArgument

--- maratonajava/src/main/java/org/acme/javacore/Oexception/domain/runtime/test/RunTimeExceptionTest02.java
+++ maratonajava/src/main/java/org/acme/javacore/Oexception/domain/runtime/test/RunTimeExceptionTest02.java
@@ -2,18 +2,25 @@ package org.acme.javacore.Oexception.domain.runtime.test;
 
 public class RunTimeExceptionTest02 {
     public static void main(String[] args) {
-        System.out.println(divisao(1, 0));
+        try {
+            divisao(1, 0);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Código finalizado");
+
     }
 
     private static int divisao(int a, int b) {
-        try {
-            return a / b;
-        } catch (RuntimeException e) {
-            e.printStackTrace();
+
+        if (b == 0) {
+            throw new IllegalArgumentException("Argumento não pode ser 0");
 
         }
+        return a / b;
 
-        return 0;
     }
 
 }
