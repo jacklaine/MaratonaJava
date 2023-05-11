4996dad5e9e378fb87fe385eae19feb04ada91e5
jacklaine <jacklainecavalheiro1@gmail.com>
2023-05-10 22:58:13 -0300

    checked

---
+++ maratonajava/src/main/java/org/acme/javacore/Oexception/domain/exception/test/ExceptionTest02.java
@@ -0,0 +1,18 @@
+package org.acme.javacore.Oexception.domain.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest02 {
+    public static void main(String[] args) throws IOException {
+        criarNovoArquivo();
+        
+    }
+
+    public static void criarNovoArquivo() throws IOException {
+        File file = new File("arquivo\\teste.txt");
+        boolean isCriado = file.createNewFile();
+        System.out.println("Arquivo criado" +isCriado);
+    }
+    
+}
